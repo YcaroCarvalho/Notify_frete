7824b8f48f3534d774e4982298e6eea57b09def4
YcaroCarvalho <gabrielsouza252007b@gmail.com>
2024-11-20 21:42:34 -0300

    Decorators para filtrar origim e destino

--- code_1.py
+++ code_1.py
@@ -5,17 +5,106 @@ from selenium.webdriver.common.by import By
 import telebot
 import time
 import pyautogui
+""""
 
 service = Service()
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
+"""
 
-URL = 'https://www.fretebras.com.br/fretes/carga-de-pr/carga-para-mg'
 TOKEN = '7178561915:AAG-hO5uBNdo8JjbQj78cB6myuao2PKIbnU'
-CHAT_ID = '-4197170094'
+CHAT_ID = '-4544372775'
 bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['help', 'start'])
+def start(message):
+    bot.reply_to(message, "-Olá, sou seu assistente que vai lhe auxiliar a encontrar os melhores fretes para voce-\n")
+    bot.reply_to(message, "De é a origim do local que vc deseja começar buscando seus fretes:\n")
+    bot.reply_to(message,'''
+        Clique no local de ORIGEM desejado:
+        -----------------------------------
+        SÃO PAULO: /Sp
+        MINAS GERAIS: /Mg
+        PARANÁ: /Pr
+        SANTA CATARINA: /Sc
+        BAHIA: /Ba
+        ACRE: /Ac   
+        ALAGOAS: /Al  
+        AMAPÁ: /Ap
+        AMAZONAS: /Am
+        CEARÁ: /Ce
+        DISTRITO FEDERAL: /Df
+        ESPIRITO SANTO: /Es
+        GOIÁS: /Go
+        MARANHÃO: /Ma
+        MATO GROSSO: /Mt
+        MATO GROSSO DO SUL: /Ms
+        PARÁ: /Pa
+        PARAÍBA: /Pb
+        RIO DE JANEIRO: /Rj
+        RIO GRANDE DO NORTE: /Rn
+        RIO GRANDE DO SUL: /Rs
+        RONDÔNIA: /Ro
+        RORAIMA: /Rr
+        SERGIPE: /Se
+        TOCANTINS: /To
+                    ''')
+                 
+      
+@bot.message_handler(commands=['Sp', 'Pr'])
+def def_origin_url(message):
+    
+    url_origim = message.text
+    bot.reply_to(message,'''
+        Clique no local de DESTINO desejado:
+        -----------------------------------
+        SÃO PAULO: /Sp
+        MINAS GERAIS: /Mg
+        PARANÁ: /Pr
+        SANTA CATARINA: /Sc
+        BAHIA: /Ba
+        ACRE: /Ac   
+        ALAGOAS: /Al  
+        AMAPÁ: /Ap
+        AMAZONAS: /Am
+        CEARÁ: /Ce
+        DISTRITO FEDERAL: /Df
+        ESPIRITO SANTO: /Es
+        GOIÁS: /Go
+        MARANHÃO: /Ma
+        MATO GROSSO: /Mt
+        MATO GROSSO DO SUL: /Ms
+        PARÁ: /Pa
+        PARAÍBA: /Pb
+        RIO DE JANEIRO: /Rj
+        RIO GRANDE DO NORTE: /Rn
+        RIO GRANDE DO SUL: /Rs
+        RONDÔNIA: /Ro
+        RORAIMA: /Rr
+        SERGIPE: /Se
+        TOCANTINS: /To
+                    ''')
+    return url_origim
+
+
+@bot.message_handler(commands=['Sp', 'Pr'])
+def def_destino_url(message):
+    
+    url_destino = message.text
+    
+    return url_destino
+
+url_origim = def_origin_url
+bot.send_message(CHAT_ID,text=f'{url_origim}')
+
+
+bot.infinity_polling()
+"""
+print (url_origim)
+URL = 'https://www.fretebras.com.br/fretes/carga-de-pr/carga-para-mg'
 driver.get(URL)
 ultimo_cod_link = [None]
+
 
 while True:
     links = driver.find_elements(By.XPATH,  '//*[@id="__next"]/main/div/fuel-grid-container/fuel-grid-item[2]/main/fuel-grid-item/div/a[1]')
@@ -29,6 +118,7 @@ while True:
         time.sleep(1)
         print(ultimo_link)
         pyautogui.click(x=871, y=381)
+        time.sleep(1)  
 
 
         def search_xpaths(xpaths):
@@ -97,16 +187,18 @@ while True:
         search_xpaths(xpaths)
 
         #ORIGEM E DESTINO
+        
         origem_city = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[1]/span/a[1]').text
         origem_estado = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[1]/span/a[2]').text
         origem = f'{origem_city} - {origem_estado}'
         origem = ('').join(origem)
-
+        
         destino_city = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[3]/span/a[1]').text
         destino_estado = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[3]/span/a[2]').text
         destino = f'{destino_city} - {destino_estado}'
         destino = ('').join(destino)
 
+        
         bot.send_message(CHAT_ID, text=f'Link: {ultimo_link}\n Produto: {produto}\n Veiculo: {veiculo}\n Origem: {origem} \n Destino: {destino}\n Carroceria: {carroceria}\n Tipo carga: {tipo_carga}')
 
         driver.back()
@@ -114,3 +206,4 @@ while True:
     else:
         time.sleep(0.5)
         driver.refresh()
+"""
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Programas\notify_frete\venv
+set VIRTUAL_ENV=E:\github\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
